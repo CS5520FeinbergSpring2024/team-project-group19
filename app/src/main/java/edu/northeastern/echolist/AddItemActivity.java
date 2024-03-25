9109c35785df1f03c850fa95847548672b990934
lliao168 <louiseliao0802@gmail.com>
2024-03-25 16:59:26 -0400

    Added recycler views to Upcoming Event and My Event to the activity_home layout based on the design we had. Added the input fields for items mentioned in the google doc to the activity_add_item layout: https://docs.google.com/document/d/1rpC6XwDrzGlgfUHqDNmwBSzSX9IE5xFyIENLQ92C4yg/edit Set up codes for adding the event data (title, location, date, userId) to Firebase when user putting data for a new event (see AddItemActivity) Set up codes for reflecting the event title and event date to the My Event recycler view (see EventAdapter and HomeActivity)

--- app/src/main/java/edu/northeastern/echolist/AddItemActivity.java
+++ app/src/main/java/edu/northeastern/echolist/AddItemActivity.java
@@ -1,12 +1,39 @@
 package edu.northeastern.echolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Calendar;
 
 public class AddItemActivity extends AppCompatActivity {
+    protected TextView userIdTextView;
+    private Button saveEventButton;
+    private EditText eventTitle;
+    private EditText eventLocation;
+    private EditText eventDate;
+    private Spinner friendsSpinner;
+    private Spinner categorySpinner;
+    private Spinner visibilitySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +44,85 @@ public class AddItemActivity extends AppCompatActivity {
         bottomNavigationView.setSelectedItemId(R.id.page_add_post);
         NavigationRouter navigationRouter = new NavigationRouter(bottomNavigationView, this);
         navigationRouter.initNavigation();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("namePref", MODE_PRIVATE);
+        String userId = sharedPreferences.getString("username", "User");
+
+        // Set the user ID to the TextView
+        userIdTextView = findViewById(R.id.userId_textview);
+        userIdTextView.setText(userId);
+
+        DatabaseReference databaseEvents = FirebaseDatabase.getInstance().getReference("events");
+
+        saveEventButton = findViewById(R.id.saveEventButton);
+        eventTitle = findViewById(R.id.eventTitle);
+        eventLocation = findViewById(R.id.eventLocation);
+        eventDate = findViewById(R.id.eventDate);
+        friendsSpinner = findViewById(R.id.friendsSpinner);
+        categorySpinner = findViewById(R.id.categorySpinner);
+        visibilitySpinner = findViewById(R.id.visibilitySpinner);
+
+        eventDate.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View v) {
+                                             showDateDialog();
+                                         }
+                                     });
+
+                saveEventButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        String title = eventTitle.getText().toString();
+                        String location = eventLocation.getText().toString();
+                        String date = eventDate.getText().toString();
+
+                        String eventId = databaseEvents.push().getKey();
+                        Event event = new Event(eventId, userId, title, location, date);
+
+
+                        databaseEvents.child(eventId).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                snapshot.getRef().setValue(event).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
+                        Intent intent = new Intent(AddItemActivity.this, HomeActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                });
+    }
+
+    private void showDateDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        String selectedDate = year + "-" + (month + 1) + "-" + dayOfMonth;
+                        eventDate.setText(selectedDate);
+                    }
+                },
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
     }
 }
---
+++ app/src/main/java/edu/northeastern/echolist/Event.java
@@ -0,0 +1,55 @@
+package edu.northeastern.echolist;
+
+public class Event {
+    private String eventId;
+    private String title;
+    private String location;
+    private String date;
+    private String userId;
+
+    public Event() {}
+
+    public Event(String eventId, String userId, String title, String location, String date) {
+        this.eventId = eventId;
+        this.userId = userId;
+        this.title = title;
+        this.location = location;
+        this.date = date;
+    }
+
+    public String getEventId() {
+        return this.eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
