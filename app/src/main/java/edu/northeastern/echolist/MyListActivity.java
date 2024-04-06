580d2b43d8954b4086d11fa98da7d120c96cda74
lliao168 <louiseliao0802@gmail.com>
2024-04-06 18:27:11 -0400

    delete the part that crash the app when clicking My List page from Home page

--- app/src/main/java/edu/northeastern/echolist/MyListActivity.java
+++ app/src/main/java/edu/northeastern/echolist/MyListActivity.java
@@ -59,57 +59,6 @@ public class MyListActivity extends AppCompatActivity {
         NavigationRouter navigationRouter = new NavigationRouter(bottomNavigationView, this);
         navigationRouter.initNavigation();
 
-        DatabaseReference databaseWishLists = FirebaseDatabase.getInstance().getReference("wishlists").child(eventId);
-
-        databaseWishLists.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                wishList.clear();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    WishListItem item = snapshot.getValue(WishListItem.class);
-                    wishList.add(item);
-                }
-                wishListAdapter.notifyDataSetChanged(); // Refresh the list
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
-                Toast.makeText(MyListActivity.this, "Failed to load data.", Toast.LENGTH_SHORT).show();
-            }
-        });
-        fabAddWishListItem.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(MyListActivity.this);
-                        builder.setTitle("Add Wish List Item");
-
-                        View view = getLayoutInflater().inflate(R.layout.activity_wishlist_item_layout, null);
-                        builder.setView(view);
-
-                        EditText nameAdd = view.findViewById(R.id.name);
-
-                        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                String name = nameAdd.getText().toString();
-                                if (!name.isEmpty()) {
-                                    String id = databaseWishLists.push().getKey();
-                                    WishListItem newItem = new WishListItem(id, name);
-                                    if (id != null) {
-                                        databaseWishLists.child(id).setValue(newItem);
-                                    }
-                                } else {
-                                    // a error dialog will show if name and url are empty
-                                    emptyEntryDialog();
-                                }
-                            }
-                        });
-                        builder.setNegativeButton("Cancel", null);
-                        builder.create().show();
-
-                    }
-                });
 
         bottomNavigationView.setSelectedItemId(R.id.page_view_posts);
         bottomNavigationView.setOnItemSelectedListener(item -> {
