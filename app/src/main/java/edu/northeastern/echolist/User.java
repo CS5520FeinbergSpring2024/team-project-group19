b16242095c8e05ff1bd75b78a971200ff4b8ef4e
zpn <zpnichols@gmail.com>
2024-04-06 20:56:17 -0400

    adding favorites attribute to user

--- app/src/main/java/edu/northeastern/echolist/User.java
+++ app/src/main/java/edu/northeastern/echolist/User.java
@@ -1,10 +1,13 @@
 package edu.northeastern.echolist;
 
+import java.util.List;
+
 public class User {
     private String userId;
     private String password;
     private String profileImageUrl;
     private String profileImageData; // Add this field for base64-encoded image data
+    private List<String> favorites;
 
 
 
@@ -21,6 +24,13 @@ public class User {
         this.userId = userId;
         this.password = password;
         this.profileImageUrl = profileImageUrl;
+    }
+
+    public User(String userId, String password, String profileImageUrl, List<String> favorites) {
+        this.userId = userId;
+        this.password = password;
+        this.profileImageUrl = profileImageUrl;
+        this.favorites = favorites;
     }
 
     public void setUserId(String userId){
@@ -50,6 +60,13 @@ public class User {
     public void setProfileImageData(String profileImageData) {
         this.profileImageData = profileImageData;
     }
+
+    public List<String> getFavorites() { return favorites; }
+
+    public void setFavorites(List<String> favorites) {
+        this.favorites = favorites;
+    }
+
 
 
 }
