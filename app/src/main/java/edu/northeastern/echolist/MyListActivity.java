f5462131d778ff417f9c454717c7f46b2ab1b78f
najordan1 <nathan.d.jordan@gmail.com>
2024-04-07 21:25:05 -0400

    clean up mylist

--- app/src/main/java/edu/northeastern/echolist/MyListActivity.java
+++ app/src/main/java/edu/northeastern/echolist/MyListActivity.java
@@ -1,32 +1,13 @@
 package edu.northeastern.echolist;
 
-import static android.content.ContentValues.TAG;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,7 +19,6 @@ public class MyListActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager lLayoutManager;
     private List<WishListItem> wishList = new ArrayList<>();
     private String eventId;
-    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,36 +34,10 @@ public class MyListActivity extends AppCompatActivity {
         wishListAdapter = new WishListAdapter(this, wishList);
         wishListRecyclerView.setAdapter(wishListAdapter);
 
+        // init navigation
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setSelectedItemId(R.id.page_view_posts);
         NavigationRouter navigationRouter = new NavigationRouter(bottomNavigationView, this);
         navigationRouter.initNavigation();
-
-
-        bottomNavigationView.setSelectedItemId(R.id.page_view_posts);
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.page_home) {
-                Intent intent = new Intent(MyListActivity.this, HomeActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-                return true;
-            } else if (item.getItemId() == R.id.page_add_post) {
-                Intent intent = new Intent(MyListActivity.this, AddItemActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-                return true;
-            }
-            return false;
-        });
-    }
-
-
-    private void emptyEntryDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Error");
-        builder.setMessage("Please enter name of the item");
-        builder.setPositiveButton("OK", null);
-        builder.create().show();
     }
 
 }
