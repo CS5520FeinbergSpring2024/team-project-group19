45d3e88a3ece92ee94324c302314909edb35fcf7
zpn <zpnichols@gmail.com>
2024-04-20 19:55:33 -0400

    adding custom gift dialog

--- app/src/main/java/edu/northeastern/echolist/GiftDialog.java
+++ app/src/main/java/edu/northeastern/echolist/GiftDialog.java
@@ -1,9 +1,13 @@
 package edu.northeastern.echolist;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,32 +15,89 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 public class GiftDialog extends DialogFragment {
 
-        private static final String ARG_DESCRIPTION = "description";
+    private static final String ARG_GIFT_ID = "giftId";
+    private static final String ARG_GIFT_NAME = "giftName";
+    private static final String ARG_DESCRIPTION = "description";
+    private static final String ARG_BUTTON_TEXT = "buttonText";
 
-        public static GiftDialog newInstance(String description) {
-            GiftDialog dialog = new GiftDialog();
-            Bundle args = new Bundle();
-            args.putString(ARG_DESCRIPTION, description);
-            dialog.setArguments(args);
-            return dialog;
+    private String giftId;
+    private String giftName;
+
+    private OnButtonClickListener onButtonClickListener;
+
+    public static GiftDialog newInstance(String giftId, String giftName, String description, String buttonText, OnButtonClickListener onButtonClickListener) {
+        GiftDialog dialog = new GiftDialog();
+        Bundle args = new Bundle();
+        args.putString(ARG_GIFT_ID, giftId);
+        args.putString(ARG_GIFT_NAME, giftName);
+        args.putString(ARG_DESCRIPTION, description);
+        args.putString(ARG_BUTTON_TEXT, buttonText);
+        dialog.setArguments(args);
+        dialog.setOnButtonClickListener(onButtonClickListener);
+        return dialog;
+    }
+
+    public void setOnButtonClickListener(OnButtonClickListener onButtonClickListener) {
+        this.onButtonClickListener = onButtonClickListener;
+    }
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_gift_details, null);
+
+        giftId = getArguments().getString(ARG_GIFT_ID);
+        giftName = getArguments().getString(ARG_GIFT_NAME);
+        String description = getArguments().getString(ARG_DESCRIPTION);
+        String buttonText = getArguments().getString(ARG_BUTTON_TEXT);
+
+        TextView giftNameTextView = view.findViewById(R.id.gift_name_textview);
+        TextView descriptionTextView = view.findViewById(R.id.description_textview);
+        Button actionButton = view.findViewById(R.id.action_button);
+
+        giftNameTextView.setText(giftName);
+        descriptionTextView.setText(description);
+        actionButton.setText(buttonText);
+
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onButtonClickListener != null) {
+                    onButtonClickListener.onButtonClick(giftId, giftName);
+                }
+            }
+        });
+
+        builder.setView(view)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+        return builder.create();
+    }
+
+    public interface OnButtonClickListener {
+        void onButtonClick(String giftId, String giftName);
+    }
+
+    public interface OnDismissListener {
+        void onDialogDismiss();
+    }
+
+    private OnDismissListener onDismissListener;
+
+    public void setOnDismissListener(OnDismissListener onDismissListener) {
+        this.onDismissListener = onDismissListener;
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialogInterface) {
+        super.onDismiss(dialogInterface);
+        if (onDismissListener != null) {
+            onDismissListener.onDialogDismiss();
         }
-
-        @NonNull
-        @Override
-        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
-            LayoutInflater inflater = requireActivity().getLayoutInflater();
-
-            String description = getArguments().getString(ARG_DESCRIPTION);
-
-            builder.setTitle("Details")
-                    .setMessage(description)
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                        }
-                    });
-
-            return builder.create();
-        }
+    }
 }
