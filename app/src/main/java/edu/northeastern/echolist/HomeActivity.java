d203bf9a1b2c7dbc13ec12acbda24c22f6a5804d
napaza147 <napaza147@live.com>
2024-04-21 16:40:32 -0400

    Pre merge

--- app/src/main/java/edu/northeastern/echolist/HomeActivity.java
+++ app/src/main/java/edu/northeastern/echolist/HomeActivity.java
@@ -8,9 +8,6 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
