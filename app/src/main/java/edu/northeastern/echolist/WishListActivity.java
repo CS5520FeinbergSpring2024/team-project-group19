97dd044f0b6ef3633248e1e9599addcd0c25efb6
zpn <zpnichols@gmail.com>
2024-04-21 10:47:45 -0400

    adding favorite gifts to wishlist activity

--- app/src/main/java/edu/northeastern/echolist/WishListActivity.java
+++ app/src/main/java/edu/northeastern/echolist/WishListActivity.java
@@ -1,38 +1,33 @@
 package edu.northeastern.echolist;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.Activity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.MutableData;
-import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -41,15 +36,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class WishListActivity extends AppCompatActivity {
     private FloatingActionButton fabAddWishListItem;
     private WishListAdapter wishListAdapter;
     private RecyclerView wishListRecyclerView;
-    private RecyclerView.LayoutManager lLayoutManager;
     private List<WishListItem> wishList = new ArrayList<>();
     private String eventId;
-    private BottomNavigationView bottomNavigationView;
-    private Toolbar toolbar;
+
+    private RecyclerView favoriteGiftsRecyclerView;
+    private GiftAdapter giftAdapter;
+    private final List<GiftItem> favoriteGifts = new ArrayList<>();
+    private GiftViewModel giftViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -181,6 +179,109 @@ public class WishListActivity extends AppCompatActivity {
                 });
         itemTouchHelper.attachToRecyclerView(wishListRecyclerView);
 
+        // favorites code
+        favoriteGiftsRecyclerView = findViewById(R.id.favorite_gifts_recyclerview);
+        Button toggleButton = findViewById(R.id.toggleButton);
+        favoriteGiftsRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        giftAdapter = new GiftAdapter(this, favoriteGifts, null, gift -> {
+            showAddToWishListDialog(gift);
+        });
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean shouldExpand = favoriteGiftsRecyclerView.getVisibility() != View.VISIBLE;
+                if (shouldExpand) {
+                    if (favoriteGifts.isEmpty()) {
+                        Toast.makeText(WishListActivity.this, "Add favorites from trending gifts on the home screen to use this feature!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        animateRecyclerViewSlide(favoriteGiftsRecyclerView, true);
+                        toggleButton.setText("Hide");
+                    }
+                } else {
+                    animateRecyclerViewSlide(favoriteGiftsRecyclerView, false);
+                    toggleButton.setText("Show");
+                }
+            }
+        });
+
+        favoriteGiftsRecyclerView.setAdapter(giftAdapter);
+
+        giftViewModel = new ViewModelProvider(this).get(GiftViewModel.class);
+
+        giftViewModel.fetchUserFavorites(getUserId());
+        Log.d("WishListActivity", "User ID: " + getUserId());
+        giftViewModel.getFavoriteGiftIds().observe(this, favoriteGiftIds -> {
+            Log.d("WishListActivity", "User favorites: " + favoriteGiftIds);
+            giftViewModel.fetchGiftItems(favoriteGiftIds);
+            giftViewModel.getGiftItemsLiveData().observe(this, giftItems -> {
+                Log.d("WishListActivity", "Gift Items: " + giftItems);
+                if (giftItems != null && favoriteGiftIds != null) {
+                    favoriteGifts.clear();
+                    for (GiftItem giftItem : giftItems) {
+                        if (favoriteGiftIds.contains(giftItem.getGiftId())) {
+                            favoriteGifts.add(giftItem);
+                        }
+                    }
+                    giftAdapter.setGiftItems(favoriteGifts);
+                }
+            });
+        });
+    }
+
+    private String getUserId() {
+        SharedPreferences sharedPreferences = getSharedPreferences("namePref", MODE_PRIVATE);
+        return sharedPreferences.getString("username", "User");
+    }
+
+    private void showAddToWishListDialog(Gift gift) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add to Wish List");
+        builder.setMessage("Do you want to add \"" + gift.getName() + "\" to the wish list?");
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String id = FirebaseDatabase.getInstance().getReference("wishlists").child(eventId).push().getKey();
+                int order = wishList.size();
+                WishListItem newItem = new WishListItem(id, gift.getName(), order);
+                if (id != null) {
+                    FirebaseDatabase.getInstance().getReference("wishlists").child(eventId).child(id).setValue(newItem);
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+        builder.create().show();
+    }
+
+    private void animateRecyclerViewSlide(final RecyclerView recyclerView, final boolean expand) {
+        int startTranslationY = expand ? recyclerView.getHeight() : 0;
+        int endTranslationY = expand ? 0 : recyclerView.getHeight();
+
+        ValueAnimator animator = ValueAnimator.ofInt(startTranslationY, endTranslationY);
+        animator.addUpdateListener(animation -> {
+            int animatedValue = (int) animation.getAnimatedValue();
+            recyclerView.setTranslationY(animatedValue);
+        });
+
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                if (expand) {
+                    recyclerView.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (!expand) {
+                    recyclerView.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.setDuration(300);
+        animator.start();
     }
 
     // Iterate over each wishlist item. Put the index of each wishlist item to a hashmap as the value
